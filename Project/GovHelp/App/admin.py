75e5f2c20048447d1e0c172ba9d915894e80086e
KrishM-codes <krishmoorjani04@gmail.com>
2024-08-20 23:42:13 +0530

    Delete Project directory

--- Project/GovHelp/App/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-@admin.register(Query)
-class QueryAdmin(admin.ModelAdmin):
-    list_display = ['id','Title','Description','Location','Status','Created','Posted_by']
